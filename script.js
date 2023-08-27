7221bff263b9eba15ea8f926030d4c33ad09724b
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-27 13:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+window.onload = function() {
+  const element = document.getElementById('level');
+  let level = 0;
+
+  function findDOMLevel(target, currentLevel) {
+    if (target.parentNode) {
+      return findDOMLevel(target.parentNode, currentLevel + 1);
+    } else {
+      return currentLevel;
+    }
+  }
+
+  if (element) {
+    level = findDOMLevel(element, 1);
+    alert("The level of the element is: " + level);
+  }
+};
